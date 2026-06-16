a1c7c4c216290f03240f7172f5da2f8f9997d43b
khoa16122004 <khoatr.work@gmail.com>
2026-06-16 21:55:53 +0700

    add

--- src/script_sh/run_attack.sh
+++ src/script_sh/run_attack.sh
@@ -1,25 +1,59 @@
 python main.py \
-    --model_name restnet_vggface \
+    --model_name arcface_ms1mv3 \
+    --baseline GA \
+    --fitness_type normal \
+    --label 0 \
+    --recons_w 0.0 \
+    --attack_w 1.0
+
+
+python main.py \
+    --model_name arcface_ms1mv3 \
+    --baseline GA \
+    --fitness_type normal \
+    --label 1 \
+    --recons_w 0.0 \
+    --attack_w 1.0
+
+
+python main.py \
+    --model_name arcface_ms1mv3 \
+    --baseline GA \
+    --fitness_type normal \
+    --label 0
+
+
+python main.py \
+    --model_name arcface_ms1mv3 \
+    --baseline GA \
+    --fitness_type normal \
+    --label 1
+
+
+
+python main.py \
+    --model_name arcface_ms1mv3 \
     --baseline GA \
     --fitness_type adaptive \
     --label 0
 
 
 python main.py \
-    --model_name restnet_vggface \
+    --model_name arcface_ms1mv3 \
     --baseline GA \
     --fitness_type adaptive \
     --label 1
 
 
+
 python main.py \
-    --model_name restnet_vggface \
+    --model_name arcface_ms1mv3 \
     --baseline NSGAII \
     --fitness_type normal \
     --label 0
 
 python main.py \
-    --model_name restnet_vggface \
+    --model_name arcface_ms1mv3 \
     --baseline NSGAII \
     --fitness_type normal \
     --label 1
