4e6246ccd4aa3610166d4678a20bdeb104d9a8d2
khoa16122004 <khoatr.work@gmail.com>
2026-06-16 14:49:07 +0700

    test

--- src/script_sh/run_attack.sh
+++ src/script_sh/run_attack.sh
@@ -1,25 +1,25 @@
 python main.py \
-    --model_name resnet_vggface \
+    --model_name restnet_vggface \
     --baseline GA \
     --fitness_type adaptive \
     --label 0
 
 
 python main.py \
-    --model_name resnet_vggface \
+    --model_name restnet_vggface \
     --baseline GA \
     --fitness_type adaptive \
     --label 1
 
 
 python main.py \
-    --model_name resnet_vggface \
+    --model_name restnet_vggface \
     --baseline NSGAII \
     --fitness_type normal \
     --label 0
 
 python main.py \
-    --model_name resnet_vggface \
+    --model_name restnet_vggface \
     --baseline NSGAII \
     --fitness_type normal \
     --label 1
