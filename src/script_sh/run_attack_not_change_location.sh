87b08c0f6fabb6ad6f84969e1c7e6b128a4d7ed6
khoa16122004 <khoatr.work@gmail.com>
2026-06-17 12:37:03 +0700

    no change location

--- src/script_sh/run_attack_not_change_location.sh
+++ src/script_sh/run_attack_not_change_location.sh
@@ -1,5 +1,34 @@
 # ================================================ arestnet_vggface
+python main.py \
+    --model_name restnet_vggface \
+    --baseline GA \
+    --fitness_type adaptive \
+    --label 0 \
+    --prob_mutate_location 0.0
 
+
+python main.py \
+    --model_name restnet_vggface \
+    --baseline GA \
+    --fitness_type adaptive \
+    --label 1 \
+    --prob_mutate_location 0.0
+
+
+
+python main.py \
+    --model_name restnet_vggface \
+    --baseline NSGAII \
+    --fitness_type normal \
+    --label 0 \
+    --prob_mutate_location 0.0
+
+python main.py \
+    --model_name restnet_vggface \
+    --baseline NSGAII \
+    --fitness_type normal \
+    --label 1 \
+    --prob_mutate_location 0.0
 
 python main.py \
     --model_name restnet_vggface \
@@ -7,7 +36,8 @@ python main.py \
     --fitness_type normal \
     --label 0 \
     --recons_w 0.0 \
-    --attack_w 1.0
+    --attack_w 1.0 \
+    --prob_mutate_location 0.0
 
 
 python main.py \
@@ -16,50 +46,28 @@ python main.py \
     --fitness_type normal \
     --label 1 \
     --recons_w 0.0 \
-    --attack_w 1.0
+    --attack_w 1.0 \
+    --prob_mutate_location 0.0
 
 
 python main.py \
     --model_name restnet_vggface \
     --baseline GA \
     --fitness_type normal \
-    --label 0
+    --label 0 \
+    --prob_mutate_location 0.0
 
 
 python main.py \
     --model_name restnet_vggface \
     --baseline GA \
     --fitness_type normal \
-    --label 1
+    --label 1 \
+    --prob_mutate_location 0.0
 
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type adaptive \
-    --label 0
 
-
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type adaptive \
-    --label 1
-
-
-
-python main.py \
-    --model_name restnet_vggface \
-    --baseline NSGAII \
-    --fitness_type normal \
-    --label 0
-
-python main.py \
-    --model_name restnet_vggface \
-    --baseline NSGAII \
-    --fitness_type normal \
-    --label 1
 
 
 # ================================================ arcface_ms1mv3
