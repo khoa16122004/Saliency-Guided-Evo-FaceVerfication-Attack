b76c526f7edda7cb4df5f4db0b0373a2f176e593
khoa16122004 <khoatr.work@gmail.com>
2026-06-18 10:57:05 +0700

    edit setup

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@ gdown 1sGtnHX3UYkg47lktNmz8dyQ8iu1hx_Ow
 unzip lfw_preprocess.zip
 rm lfw_preprocess.zip
 
+# mkdiar
+mkdir src/pretrained_model
+
 # pretrained model
 wget -O src/pretrained_model/vggface2.pt https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180402-114759-vggface2.pt
 wget -O src/pretrained_model/webface.pt https://github.com/timesler/facenet-pytorch/releases/download/v2.2.9/20180408-102900-casia-webface.pt
-
-
-
-
+gdown -O src/pretrained_model/arcface.pth 1Hmqf25ZIoVLng3wrg0CgTwzA93XQmRCc
+gdown -O src/pretrained_model/cosface.pth 1hlVcK4mc3kGp4FU_slRVJJD-YIcw5lNr
