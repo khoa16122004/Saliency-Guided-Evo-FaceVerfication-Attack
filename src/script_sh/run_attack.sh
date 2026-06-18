091775252a6c084eb41f594d75acc8f60b7866d5
khoa16122004 <khoatr.work@gmail.com>
2026-06-18 11:02:02 +0700

    add sbatch run

--- src/script_sh/run_attack.sh
+++ src/script_sh/run_attack.sh
@@ -1,65 +1,65 @@
 # ================================================ arestnet_vggface
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type normal \
-    --label 0 \
-    --recons_w 0.0 \
-    --attack_w 1.0
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 0 \
+#     --recons_w 0.0 \
+#     --attack_w 1.0
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type normal \
-    --label 1 \
-    --recons_w 0.0 \
-    --attack_w 1.0
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1 \
+#     --recons_w 0.0 \
+#     --attack_w 1.0
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type normal \
-    --label 0
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 0
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type normal \
-    --label 1
-
-
-
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type adaptive \
-    --label 0
-
-
-python main.py \
-    --model_name restnet_vggface \
-    --baseline GA \
-    --fitness_type adaptive \
-    --label 1
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1
 
 
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline NSGAII \
-    --fitness_type normal \
-    --label 0
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 0
 
-python main.py \
-    --model_name restnet_vggface \
-    --baseline NSGAII \
-    --fitness_type normal \
-    --label 1
+
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 1
+
+
+
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 0
+
+# python main.py \
+#     --model_name restnet_vggface \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 1
 
 
 # ================================================ arcface_ms1mv3
@@ -120,6 +120,68 @@ python main.py \
 
 # python main.py \
 #     --model_name arcface_ms1mv3 \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 1
+
+# ================================================ cosface_glint360k
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 0 \
+#     --recons_w 0.0 \
+#     --attack_w 1.0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1 \
+#     --recons_w 0.0 \
+#     --attack_w 1.0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1
+
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 1
+
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 0
+
+# python main.py \
+#     --model_name cosface_glint360k \
 #     --baseline NSGAII \
 #     --fitness_type normal \
 #     --label 1
---
+++ src/script_sh/run_sbatch_attack.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#SBATCH --job-name=BCOS_PGD
+#SBATCH --output=PGD/mps_%j.out
+#SBATCH --error=PGD/mps_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=mps:a100:2 # Không khai báo GPU; --gres=mps:l40:2 (card L40); --gres=mps:a100:2 (card A100, ưu tiên các job dùng > 40GB vRAM)
+#SBATCH --mem=4G
+#SBATCH --time=72:00:00
+REQUIRED_VRAM=20000  # Quan trọng - Số vRAM cần dùng (để tìm GPU phù hợp)
+# =========================================================
+# CHUẨN BỊ MÔI TRƯỜNG
+# =========================================================
+module clear -f
+# *** Kích hoạt venv (Sửa đường dẫn / môi trường theo user)
+cd /datastore/elo/khoatn/BCOS_ATTACK/script
+source /home/elo/miniconda3/etc/profile.d/conda.sh
+conda activate bcos_attack
+echo "ENV:" $CONDA_DEFAULT_ENV
+echo "PREFIX:" $CONDA_PREFIX
+which python
+python -c "import sys; print(sys.executable)"
+
+# Xóa biến môi trường Slurm để tự chọn GPU
+unset CUDA_VISIBLE_DEVICES
+# --- GỌI HELPER --- (Quan trọng, cần gọi hàm này (có sẵn) để tìm GPU có vRAM trống >= REQUIRED_VRAM, nếu không tìn thấy GPU đủ vRAM thì hàm CHECK_OUT sẽ đưa job vào lại hàng đợi để chờ tìm slot khác; sau 5 lần requeue mà vẫn chưa tìm được slot thì sẽ trả về mã lỗi để kết thúc job)
+CHECK_OUT=$(/usr/local/bin/gpu_check.sh $REQUIRED_VRAM $SLURM_JOB_ID)
+EXIT_CODE=$?
+if [ $EXIT_CODE -eq 10 ]; then
+    echo "$CHECK_OUT"
+    exit 0 
+elif [ $EXIT_CODE -eq 11 ]; then
+    echo "$CHECK_OUT"
+    exit 1 
+fi
+BEST_GPU=$CHECK_OUT
+echo "✅ Job $SLURM_JOB_ID bắt đầu trên GPU: $BEST_GPU"
+
+
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps-job$SLURM_JOB_ID
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-mps-log-job$SLURM_JOB_ID
+
+rm -rf $CUDA_MPS_PIPE_DIRECTORY $CUDA_MPS_LOG_DIRECTORY
+mkdir -p $CUDA_MPS_PIPE_DIRECTORY $CUDA_MPS_LOG_DIRECTORY
+
+export CUDA_VISIBLE_DEVICES=$BEST_GPU 
+
+# =========================================================
+# CHẠY CODE
+# =========================================================
+
+#!/bin/bash
+
+python main.py \
+    --model_name cosface_glint360k \
+    --baseline GA \
+    --fitness_type normal \
+    --label 0 \
+    --recons_w 0.0 \
+    --attack_w 1.0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1 \
+#     --recons_w 0.0 \
+#     --attack_w 1.0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type normal \
+#     --label 1
+
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 0
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline GA \
+#     --fitness_type adaptive \
+#     --label 1
+
+
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 0
+
+# python main.py \
+#     --model_name cosface_glint360k \
+#     --baseline NSGAII \
+#     --fitness_type normal \
+#     --label 1
+
